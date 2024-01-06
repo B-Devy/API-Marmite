713d961c05520af301695c36cd0d28bf9d8fc8eb
B-Devy <bertrandcafolla@hotmail.com>
2024-01-06 02:41:25 +0100

    0.0.4

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ connectDb()
     }))
     .catch(err => console.log(err));
 
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+    });
 
 app.use(express.json());  ///--- fondamentale pour echanger les models sous forme json avec mongodb
 
